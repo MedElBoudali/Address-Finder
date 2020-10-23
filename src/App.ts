5aa3246b7e366d46f164cc7ff4bdbf1156805f65
MedEBoudali <med.sneaky@gmail.com>
2020-10-23 23:09:03 +0000

    add axios

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const form = document.querySelector('form')! as HTMLFormElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAdrressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+}
+
+form.addEventListener('submit', searchAdrressHandler);
