8f6c1de24690bf77e79d21fdbe91d41a09d80cca
MedEBoudali <med.sneaky@gmail.com>
2020-10-24 00:17:40 +0000

    add results type

--- src/App.ts
+++ src/App.ts
@@ -8,13 +8,16 @@ const searchAdrressHandler = (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
   axios
-    .get<{ results: { geometry: { location: { lat: number; lng: number } } }[], status: string }>(
+    .get<{
+      results: { geometry: { location: { lat: number; lng: number } } }[];
+      status: 'OK' | 'ZERO_RESULTS';
+    }>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
     .then(response => {
-      if (response.data.status !== 'ok') throw new Error('Could not fetch location!');
+      if (response.data.status !== 'OK') throw new Error('Could not fetch location!');
       const coordinates = response.data.results[0].geometry.location;
     })
     .catch(error => console.log(error));
