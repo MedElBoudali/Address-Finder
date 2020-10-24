95c8863a5675eaa2d42a4153e9c45efa93b3decb
MedEBoudali <med.sneaky@gmail.com>
2020-10-24 00:11:54 +0000

    add results type

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,23 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+const GOOGLE_API_KEY = 'YOUR API KEY';
 
-function searchAdrressHandler(event: Event) {
+const searchAdrressHandler = (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-  console.log(enteredAddress);
-}
+  axios
+    .get<{ results: { geometry: { location: { lat: number; lng: number } } }[], status: string }>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== 'ok') throw new Error('Could not fetch location!');
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch(error => console.log(error));
+};
 
-form.addEventListener('submit', searchAdrressHandler);
+form.addEventListener('submit', e => searchAdrressHandler(e));
